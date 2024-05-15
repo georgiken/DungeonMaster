6cfb57bc2e7086e1f506927e78ef0c3548d67455
georgiken <138819024+georgiken@users.noreply.github.com>
2024-05-15 06:02:59 +0300

    Create API.php
    init

---
+++ API.php
@@ -0,0 +1,34 @@
+<?php
+
+class DungeonAPI {
+    private $dungeon;
+
+    public function __construct($mapData, $startRoomIndex) {
+        $this->dungeon = new Dungeon($mapData, $startRoomIndex);
+    }
+
+    public function startGame() {
+        echo "Игра началась!\n";
+        echo "Вы находитесь в стартовой комнате.\n";
+    }
+
+    public function movePlayer($direction) {
+        try {
+            $this->dungeon->movePlayer($direction);
+            echo "Вы переместились в направлении $direction.\n";
+        } catch (Exception $e) {
+            echo $e->getMessage() . "\n";
+        }
+    }
+
+    public function endGame() {
+        $finalScore = $this->dungeon->getFinalScore();
+        $shortestPath = $this->dungeon->getShortestPath();
+
+        echo "Игра окончена!\n";
+        echo "Ваш итоговый счет: $finalScore.\n";
+        echo "Кратчайший путь прохождения подземелья: " . implode(' -> ', $shortestPath) . ".\n";
+    }
+}
+
+?>
